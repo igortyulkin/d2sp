8fb7b2837219b0c87112027f2be67d48f9cfcc70
TYULKIN Igor <Igor.TYULKIN@raiffeisen.ru>
2024-10-05 07:39:04 +0300

    task6 add icons

--- frontend/src/components/Logout.tsx
+++ frontend/src/components/Logout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button} from 'vienna-ui'
 import {ApplicationUserStorageFactory} from "../common/user/ApplicationUserStorage";
+import {Exit} from "vienna.icons";
 
 export const Logout = (props: {disabled?:boolean}) => {
     const handleClick = () => {
@@ -8,9 +9,9 @@ export const Logout = (props: {disabled?:boolean}) => {
         window.location.replace('/');
     }
     return (
-        <div style={{height: '5rem', minWidth: "70%"}}>
+        <div style={{minWidth: "70%"}}>
             <Button disabled={props.disabled == true} design="primary" onClick={handleClick} style={{float: "right"}}>
-                Logout
+                <Exit/>Logout
             </Button>
         </div>
     )
--- frontend/src/components/NavHeader.tsx
+++ frontend/src/components/NavHeader.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Tabs} from 'vienna-ui'
 import {Logout} from "./Logout";
 import {Link} from "react-router-dom";
-import {Badge} from "vienna-ui";
+import {Badge,Tabs,Flex} from "vienna-ui";
+import {Home, PersonLevel, MoneyBag, CashTransactions, Premium} from "vienna.icons";
+
 
 export const NavHeader = (props: { disabled?: boolean }) => {
     const resolveActive = (urlPart: string) => {
@@ -10,21 +11,23 @@ export const NavHeader = (props: { disabled?: boolean }) => {
     }
     return (
         <div className="App">
-            <h1>DS2P prediction service</h1>
+            <Flex>
+                <h1>DS2P prediction service</h1>
+                <Logout disabled={props.disabled}/>
+            </Flex>
             <div style={{height: '5rem'}}>
                 <Tabs design="primary">
                     <Tabs.Tab active={resolveActive('home')} value={"home"}><Link
-                        to={'/home'}><Badge>Home</Badge></Link></Tabs.Tab>
+                        to={'/home'}><Badge><Home/>Home</Badge></Link></Tabs.Tab>
                     <Tabs.Tab active={resolveActive('profile')} disabled={props.disabled} value={"profile"}><Link
-                        to={'/profile'}><Badge>My profile</Badge></Link></Tabs.Tab>
+                        to={'/profile'}><Badge><PersonLevel/>My profile</Badge></Link></Tabs.Tab>
                     <Tabs.Tab active={resolveActive('balance')} disabled={props.disabled} value={"balance"}><Link
-                        to={'/balance'}><Badge>Balance</Badge></Link></Tabs.Tab>
+                        to={'/balance'}><Badge><MoneyBag/>Balance</Badge></Link></Tabs.Tab>
                     <Tabs.Tab active={resolveActive('transaction')} disabled={props.disabled}
                               value={"transactions"}><Link
-                        to={'/transaction'}><Badge>Transactions</Badge></Link></Tabs.Tab>
+                        to={'/transaction'}><Badge><CashTransactions/>Transactions</Badge></Link></Tabs.Tab>
                     <Tabs.Tab active={resolveActive('prediction')} disabled={props.disabled} value={"predicts"}><Link
-                        to={'/prediction'}><Badge>Predictions</Badge></Link></Tabs.Tab>
-                    <Logout disabled={props.disabled}/>
+                        to={'/prediction'}><Badge><Premium/>Predictions</Badge></Link></Tabs.Tab>
                 </Tabs>
             </div>
         </div>
